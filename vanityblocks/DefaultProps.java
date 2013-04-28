c6c878ba26eff1b47bebbd642d1dceb197a8d4f6
Anarchy <Anarchysage@mcforums.com>
2013-04-28 04:15:59 -0500

    Erase of old files

--- vanityblocks/DefaultProps.java
+++
@@ -1,9 +0,0 @@
-package vanityblocks;
-
-public class DefaultProps {
-
-    public static final String FILE_CONFIG = "config/vanityblocks/VanityBlocks.cfg";
-    public static final String VERSIONStorage = "3.2.2";
-    public static final String VERSIONTE = "3.2.2";
-    public static final String VERSIONVanity = "3.2.2";
-}
